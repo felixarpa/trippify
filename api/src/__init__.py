2efdf5fe69fd3880814b20acdbb564909d37de7e
Albert Suarez <alsumo95@gmail.com>
2019-02-02 19:43:16 +0000

    Added SQLAlchemy initialization

---
+++ api/src/__init__.py
@@ -0,0 +1,15 @@
+TRIPPIFY_DB_USER = 'trippify'
+TRIPPIFY_DB_PASSWORD = 'trippify1234'
+TRIPPIFY_DB_DB = 'trippify'
+TRIPPIFY_DB_HOST = 'localhost'  # Python module execution.
+# TRIPPIFY_DB_HOST = 'trippify-db'  # Docker execution.
+TRIPPIFY_DB_PORT = 5432
+
+
+__all__ = [
+    'TRIPPIFY_DB_USER',
+    'TRIPPIFY_DB_PASSWORD',
+    'TRIPPIFY_DB_DB',
+    'TRIPPIFY_DB_HOST',
+    'TRIPPIFY_DB_PORT'
+]
