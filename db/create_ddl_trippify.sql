292bee25569ee1b6bd95bf52be1d3d4ed8c6de14
Albert Suarez <alsumo95@gmail.com>
2019-02-02 19:12:44 +0000

    Added more data in trippify SQL script

--- db/create_ddl_trippify.sql
+++ db/create_ddl_trippify.sql
@@ -50,3 +50,58 @@ TABLESPACE pg_default;
 
 ALTER TABLE trippify_trip
     OWNER to trippify;
+
+
+CREATE TABLE trippify_participant
+(
+    id integer NOT NULL DEFAULT nextval('trippify_participant_id_seq'::regclass),
+    trip_id integer NOT NULL,
+    name character varying(50) COLLATE pg_catalog."default",
+    origin character varying(500) COLLATE pg_catalog."default",
+    music_genre character varying(50) COLLATE pg_catalog."default",
+    CONSTRAINT trippify_participant_pkey PRIMARY KEY (id),
+    CONSTRAINT trippify_participant_trip_fkey FOREIGN KEY (trip_id)
+        REFERENCES trippify_trip (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE trippify_participant
+    OWNER to trippify;
+
+
+CREATE TABLE trippify_car
+(
+    id integer NOT NULL DEFAULT nextval('trippify_car_id_seq'::regclass),
+    participant_id integer NOT NULL,
+    car_name character varying(50) COLLATE pg_catalog."default",
+    car_brand character varying(50) COLLATE pg_catalog."default",
+    car_model character varying(50) COLLATE pg_catalog."default",
+    car_available_seats integer,
+    CONSTRAINT trippify_car_pkey PRIMARY KEY (id),
+    CONSTRAINT trippify_car_participant_fkey FOREIGN KEY (participant_id)
+        REFERENCES trippify_participant (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE trippify_car
+    OWNER to trippify;
+
+-- Insert values
+insert into trippify_trip values (1, 'Royal Hackaway 2019', 'Best hackathon ever', 'Windsor Building, University in Egham, England', 'EUR');
+
+insert into trippify_participant values (1, 1, 'Bernat', 'Big Ben', 'Rock');
+insert into trippify_participant values (2, 1, 'Albert', 'Candem Town', 'Pop');
+insert into trippify_participant values (3, 1, 'Felix', 'Liverpool Street, London', 'Punk');
+insert into trippify_participant values (4, 1, 'Elena', 'Waterloo Station, London', 'Metal');
+
+insert into trippify_car values (1, 1, 'Tixerino', 'Renault', 'Clio', 4);
