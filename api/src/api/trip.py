d7f79adfa4b25f2b53fed06e10753d05c354ed37
Albert Suarez <alsumo95@gmail.com>
2019-02-02 20:14:17 +0000

    Add retrieve trip function

--- api/src/api/trip.py
+++ api/src/api/trip.py
@@ -1,5 +1,15 @@
+from flask import jsonify
+
+from src.db.sqlalchemy import db_session
+from src.model.trip import Trip
+
+
 def get(trip_id):
-    pass
+    trip = db_session().query(Trip).filter_by(id=trip_id).first()
+    if trip:
+        return jsonify(error=False, response=trip.serialize()), 200
+    else:
+        return jsonify(error=True, message='No trip found with {} as id.'.format(trip_id)), 200
 
 
 def post():
