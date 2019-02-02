e5e0a2125c32412601bd7d31a2613c20265fac92
Albert Suarez <alsumo95@gmail.com>
2019-02-02 20:55:59 +0000

    Refactored run.sh order for initializing the DB before running the stack

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 echo '🚀 [Trippify] Shutting down last containers'
 docker-compose down
-echo '🚀 [Trippify] Building and kicking off containers'
-docker-compose up -d --build
+echo '🚀 [Trippify] Building and kicking off the PostgresSQL DB container'
+docker-compose up -d --build trippify-db
 echo '🚀 [Trippify] Sleeping 5 seconds for letting the DB initializes'
 sleep 5
 echo '🚀 [Trippify] Creating DDL Base'
@@ -11,4 +11,6 @@ echo '🚀 [Trippify] DDL Base created'
 echo '🚀 [Trippify] Creating DDL Trippify'
 docker run -it --rm --network trippify trippify-db psql -h trippify-db -U trippify trippify -f /tmp/create_ddl_trippify.sql
 echo '🚀 [Trippify] DDL Trippify created'
+echo '🚀 [Trippify] Building and kicking off the Backend and Frontend containers'
+docker-compose up -d --build trippify-api-app trippify-client-app
 echo '🚀 [Trippify] Deployed!'
