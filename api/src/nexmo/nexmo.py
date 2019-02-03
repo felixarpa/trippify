93c7b95038c6d450392b939bf34d99c8a2db16f2
Albert Suarez <alsumo95@gmail.com>
2019-02-03 10:14:12 +0000

    Removed trip_id for simplicity

--- api/src/nexmo/nexmo.py
+++ api/src/nexmo/nexmo.py
@@ -6,9 +6,9 @@ from src import *
 nexmo_client = nexmo.Client(key=NEXMO_CLIENT_KEY, secret=NEXMO_CLIENT_SECRET)
 
 
-def send_message(phone_number, trip_id):
+def send_message(phone_number):
     nexmo_client.send_message({
         'from': NEXMO_FROM,
         'to': str(phone_number),
-        'text': 'Join the Trippify family going to this trip! https://mytrippify.com/trip/{}'.format(trip_id)
+        'text': 'Join the Trippify family going to this trip! https://mytrippify.com/'
     })
