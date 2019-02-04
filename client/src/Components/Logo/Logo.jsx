a1bee2b51609ab4da0203d4044e07530c68a1707
elena20ruiz <elena.ruiz.bdn@gmail.com>
2019-02-04 22:41:50 +0100

    Add style to first form

---
+++ client/src/Components/Logo/Logo.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Logo.css';
+
+class Logo extends Component {
+
+  render() {
+    return (
+        <p className='logo'> trippify!</p>
+    );
+  }
+}
+
+export default Logo;
